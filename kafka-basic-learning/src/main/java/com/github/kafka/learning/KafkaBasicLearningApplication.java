afc8c9568e0deb1f4481a5687b89b661daa9f0e7
Pritesh Gavali <gavali.pritesh@gmail.com>
2020-08-10 19:22:22 +0530

    Initial commit - project creation

---
+++ kafka-basic-learning/src/main/java/com/github/kafka/learning/KafkaBasicLearningApplication.java
@@ -0,0 +1,12 @@
+package com.github.kafka.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBasicLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBasicLearningApplication.class, args);
+	}
+}
---
+++ kafka-basic-learning/src/test/java/com/github/kafka/learning/KafkaBasicLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kafka.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBasicLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
